bec4cfcefea647bad99f64331c33db32ba6e90bd
Suman Das <sum4ndas@gmail.com>
2024-08-01 10:13:18 +0530

    Write db queries for items

---
+++ db/itemQueries.js
@@ -0,0 +1,59 @@
+const pool = require("./db");
+
+async function getAllItems() {
+  const { rows } = await pool.query(
+    "SELECT * FROM items JOIN slots ON slots.slot_id=items.slot_id JOIN item_types ON item_types.item_type_id=items.item_type_id"
+  );
+  return rows;
+}
+
+async function getItem(id) {
+  const { rows } = await pool.query(
+    "SELECT * FROM items JOIN slots ON slots.slot_id=items.slot_id JOIN item_types ON item_types.item_type_id=items.item_type_id WHERE items.item_id=($1)",
+    [id]
+  );
+  return rows[0];
+}
+
+async function insertItem(
+  name,
+  item_type_id,
+  slot_id,
+  quality,
+  ilvl,
+  description,
+  image_url
+) {
+  await pool.query(
+    "INSERT INTO items(name, item_type_id, slot_id, quality, ilvl, description, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+    [name, item_type_id, slot_id, quality, ilvl, description, image_url]
+  );
+}
+
+async function updateItem(
+  id,
+  name,
+  item_type_id,
+  slot_id,
+  quality,
+  ilvl,
+  description,
+  image_url
+) {
+  await pool.query(
+    "UPDATE items SET name = $1, item_type_id = $2, slot_id = $3, quality = $4, ilvl = $5, description = $6, image_url = $7 WHERE item_id = $8",
+    [name, item_type_id, slot_id, quality, ilvl, description, image_url, id]
+  );
+}
+
+async function deleteItem(id) {
+  await pool.query("DELETE FROM items WHERE item_id = $1", [id]);
+}
+
+module.exports = {
+  getAllItems,
+  getItem,
+  insertItem,
+  updateItem,
+  deleteItem,
+};
