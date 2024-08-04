29011bd2ca4f28560b6a936c5ca03b4eb9a9dd11
Suman Das <sum4ndas@gmail.com>
2024-08-04 07:24:08 +0530

    Update populatedb.js - update items table

--- db/populatedb.js
+++ db/populatedb.js
@@ -3,6 +3,11 @@
 const { Client } = require("pg");
 
 const SQL = `
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS item_types;
+DROP TABLE IF EXISTS slots;
+DROP TYPE IF EXISTS item_level;
+
 CREATE TABLE IF NOT EXISTS item_types(
   item_type_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   type_name VARCHAR (50) UNIQUE NOT NULL
@@ -14,15 +19,18 @@ CREATE TABLE IF NOT EXISTS slots(
   slot_price INT NOT NULL
 );
 
+CREATE TYPE item_level AS ENUM ('251', '264');
+
 CREATE TABLE IF NOT EXISTS items(
   item_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR (255) UNIQUE NOT NULL,
   item_type_id INT,
   slot_id INT,
-  quality VARCHAR (50),
-  ilvl INT,
+  quality VARCHAR (50) NOT NULL DEFAULT 'Epic',
+  check (quality = 'Epic'),
+  ilvl item_level,
   description TEXT,
-  image_url VARCHAR (255),
+  image_url VARCHAR (255) DEFAULT '#',
   FOREIGN KEY (item_type_id) REFERENCES item_types(item_type_id),
   FOREIGN KEY (slot_id) REFERENCES slots(slot_id)
 );
@@ -54,10 +62,10 @@ VALUES
 ('Misc', 23),
 ('Thrown', 30);
 
-INSERT INTO items(name, item_type_id, slot_id, quality, ilvl, image_url)
+INSERT INTO items(name, item_type_id, slot_id, ilvl, image_url)
 VALUES
-('Belt of Omission', 1, 4, 'Epic', 264, 'https://static.wikia.nocookie.net/wowwiki/images/6/6d/Inv_belt_66.png/revision/latest/scale-to-width-down/32?cb=20091028035143'),
-('Cat Burglar''s Grips', 4, 6, 'Epic', 264, 'https://static.wikia.nocookie.net/wowwiki/images/6/6c/Inv_gauntlets_79.png/revision/latest?cb=20090708233821');
+('Belt of Omission', 1, 4, '264', 'https://static.wikia.nocookie.net/wowwiki/images/6/6d/Inv_belt_66.png/revision/latest/scale-to-width-down/32?cb=20091028035143'),
+('Cat Burglar''s Grips', 4, 6, '264', 'https://static.wikia.nocookie.net/wowwiki/images/6/6c/Inv_gauntlets_79.png/revision/latest?cb=20090708233821');
 `;
 
 async function main() {
