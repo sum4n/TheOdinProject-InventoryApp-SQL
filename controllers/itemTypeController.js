8355acd8a05be954d9800b2e886497c4ea2b3dea
Suman Das <sum4ndas@gmail.com>
2024-08-05 08:06:47 +0530

    Update itemTypeController to validate form data

--- controllers/itemTypeController.js
+++ controllers/itemTypeController.js
@@ -1,5 +1,20 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 const db = require("../db/itemTypeQueries");
+
+const isLetterHyphenApostrophe = (value) => {
+  return /^[A-Za-z\s'-]+$/.test(value);
+};
+const validateItemTypeData = [
+  body("type_name")
+    .trim()
+    .isLength({ min: 1, max: 50 })
+    .withMessage("Item type name must be between 1 - 50 characters")
+    .custom(isLetterHyphenApostrophe)
+    .withMessage(
+      "Item type name can only include a-z characters, hyphen and apostrophe."
+    ),
+];
 
 exports.listItemTypes = asyncHandler(async (req, res) => {
   let itemTypes = await db.getAllItemTypes();
@@ -23,11 +38,25 @@ exports.createItemType_get = asyncHandler(async (req, res) => {
   });
 });
 
-exports.createItemType_post = asyncHandler(async (req, res) => {
-  const itemTypeName = req.body.type_name;
-  await db.insertItemType(itemTypeName);
-  res.redirect("/item_types");
-});
+exports.createItemType_post = [
+  validateItemTypeData,
+  asyncHandler(async (req, res) => {
+    const type_name = req.body.type_name;
+    const itemType = { type_name };
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("pages/item_types/itemTypeForm", {
+        title: "Create Item Type",
+        itemType,
+        errors: errors.array(),
+      });
+    }
+
+    await db.insertItemType(itemTypeName);
+    res.redirect("/item_types");
+  }),
+];
 
 exports.updateItemType_get = asyncHandler(async (req, res) => {
   let itemType = await db.getItemType(req.params.id);
@@ -37,13 +66,27 @@ exports.updateItemType_get = asyncHandler(async (req, res) => {
   });
 });
 
-exports.updateItemType_post = asyncHandler(async (req, res) => {
-  const type_name = req.body.type_name;
-  // Don't get id from body, it can be changed by making the input field visible.
-  const id = req.params.id;
-  await db.updateItemType(id, type_name);
-  res.redirect(`/item_types/${id}`);
-});
+exports.updateItemType_post = [
+  validateItemTypeData,
+  asyncHandler(async (req, res) => {
+    const type_name = req.body.type_name;
+    const itemType = { type_name };
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("pages/item_types/itemTypeForm", {
+        title: "Update Item Type",
+        itemType,
+        errors: errors.array(),
+      });
+    }
+
+    // Don't get id from body, it can be changed by making the input field visible.
+    const id = req.params.id;
+    await db.updateItemType(id, type_name);
+    res.redirect(`/item_types/${id}`);
+  }),
+];
 
 exports.deleteItemType_get = asyncHandler(async (req, res) => {
   const id = req.params.id;
