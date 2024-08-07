2df3bc59af374b55afd35b7cfe1ad9eb06ccbc51
Suman Das <sum4ndas@gmail.com>
2024-08-07 12:03:22 +0530

    Update/changes for database deployment to Neon

--- db/db.js
+++ db/db.js
@@ -1,11 +1,29 @@
+require("dotenv").config();
+
 const { Pool } = require("pg");
 
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
 });
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 
 module.exports = pool;
